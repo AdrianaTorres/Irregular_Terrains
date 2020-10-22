3973d4635d9bf0c74b2b4f104f3b7d86ef122d72
AdrianaTorres <35779332+AdrianaTorres@users.noreply.github.com>
2020-10-22 14:37:29 +0200

    Read/write functions

---
+++ Ratio_Index_octave/src/store_result.m
@@ -0,0 +1,30 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% store_scalar.m
+%
+% Store a scalar into yaml file
+%
+% Anthony Remazeilles & Jawad Masood
+% Copyright Tecnalia CTAG 2020 (We may discuss this)
+% Beerware license.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function is_ok = store_result(filename, pi, data, title)
+
+    file_id = fopen(filename, "w");
+    fprintf(file_id, pi);
+    
+    value_str = sprintf("[[%s], [", title);
+    fprintf(file_id, value_str);
+    for i = 1:size(data)(2)
+        if (i==size(data)(2))
+          value_str = sprintf("%.5f]]", data(i));
+          fprintf(file_id, value_str);
+        else
+        value_str = sprintf("%.5f, ", data(i));
+        fprintf(file_id, value_str);
+        endif
+    endfor
+    
+    fclose(file_id);
+    is_ok = true;
+end
